b27760335d8057272017f071bc87885fab8e5601
Chaphasilor <ppp.u@web.de>
2021-04-10 22:14:03 +0200

    don't hide the link info spinner completely :)

--- src/components/LinkRow.vue
+++ src/components/LinkRow.vue
@@ -35,12 +35,12 @@
       >
 
         <span
-          v-if="!loadingLinkInfo"
+          v-if="!loadingLinkInfo || (meta !== {} && size > -1)"
           class=""
         >{{ formattedSize }}</span>
 
         <svg
-          v-if="loadingLinkInfo && (meta !== {} && size > -1)"
+          v-if="loadingLinkInfo && (meta === {} && size === -1)"
           key="0"
           class="w-6 h-6 animate-spin"
           viewBox="0 0 24 24"
